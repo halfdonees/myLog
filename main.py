ad140bf17b1351dd2fe8907dc0f0835039decb15
halfdonees <40475027h@gapps.ntnu.edu.tw>
2020-02-09 18:33:28 +0800

    fix comment bugs

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 
 
 from myLog import configure_logger
+
+#配置logger，name為預設的'default'
 #這行要放在其他需要logger的module前面
 configure_logger(console_level=logging.INFO)
 
 import module
 
 
-#配置logger，name為預設的'default'
 
 #取得name為'default'的logger
 logger = logging.getLogger('default')
